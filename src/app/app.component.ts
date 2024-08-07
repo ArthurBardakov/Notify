8d4ceb0042ed9e8305d19246eca7c00f4858e73e
ArthurBardakov <artleks@gmail.com>
2024-08-07 13:25:29 +0300

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,30 +2,25 @@ import {
   AfterViewInit,
   Component,
   computed,
-  effect,
   ElementRef,
-  inject,
   signal,
   viewChild,
 } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './navbar/navbar.component';
 import Hammer from 'hammerjs';
 // do not remove the following import - tests complain if it's not there
 import HammerInput from 'hammerjs';
 import { MenuIcons } from './navbar/icons.enum';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { NewNoteComponent } from './new-note/new-note.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, MatDialogModule, NewNoteComponent],
+  imports: [NavBarComponent, NewNoteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements AfterViewInit {
-  private readonly dialog = inject(MatDialog);
   private readonly contentElement = viewChild.required<ElementRef<HTMLElement>>('content');
   public readonly swipeLeft = signal<HammerInput | undefined>(undefined);
   public readonly swipeRight = signal<HammerInput | undefined>(undefined);
