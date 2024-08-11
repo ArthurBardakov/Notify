4119a0c523c931c7cbbdf567c20578e22ed69f82
ArthurBardakov <artleks@gmail.com>
2024-08-11 17:39:00 +0300

    add base menu component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,18 @@
 import { Routes } from '@angular/router';
+import { NotifyRoutes } from './shared/enums/routes';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'list', pathMatch: 'full' },
   {
-    path: 'list',
+    path: NotifyRoutes.Menu,
+    loadComponent: () => import('./content/menu/menu.component').then((m) => m.MenuComponent),
+  },
+  {
+    path: NotifyRoutes.List,
     loadComponent: () => import('./content/list/list.component').then((m) => m.ListComponent),
   },
   {
-    path: 'new-note',
+    path: NotifyRoutes.NewNote,
     loadComponent: () => import('./content/new-note/new-note.component').then((m) => m.NewNoteComponent),
   },
 ];
