60d8d34a0f4149d4810b02b625b6b99ad00c049d
ArthurBardakov <artleks@gmail.com>
2024-08-12 18:55:59 +0300

    add base new note storage logic

--- src/app/content/new-note/new-note.component.ts
+++ src/app/content/new-note/new-note.component.ts
@@ -1,12 +1,13 @@
-import { Component, inject } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Component, effect, HostListener, inject, OnDestroy, untracked } from '@angular/core';
+import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { NoteBottomNavbarComponent } from './note-bottom-navbar/note-bottom-navbar.component';
-import { NavService } from '../../navbar/nav.service';
-import { NotifyRoutes } from '../../shared/enums/routes';
 import { NotesStore } from '../../state/notes.store';
 import { v4 as uuidv4 } from 'uuid';
+import { merge } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { Note } from '../../shared/models/note';
 
 @Component({
   selector: 'app-new-note',
@@ -15,23 +16,52 @@ import { v4 as uuidv4 } from 'uuid';
   standalone: true,
   imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, NoteBottomNavbarComponent],
 })
-export class NewNoteComponent {
-  private readonly navSrc = inject(NavService);
+export class NewNoteComponent implements OnDestroy {
   private readonly store = inject(NotesStore);
-  public readonly noteTitle = new FormControl('');
-  public readonly noteContent = new FormControl('');
+  private readonly nonNullBuilder = inject(NonNullableFormBuilder);
+  public readonly noteTitle = this.nonNullBuilder.control('');
+  public readonly noteContent = this.nonNullBuilder.control('');
+  
+  private readonly currentNote: Note = {
+    id: uuidv4(),
+    title: '',
+    content: '',
+    createdAt: new Date(),
+    updatedAt: null,
+  };
+
+  private readonly noteUpdated = toSignal(merge(
+    this.noteTitle.valueChanges,
+    this.noteContent.valueChanges));
 
   constructor() {
-    this.store.addNote({
-      id: uuidv4(),
-      title: '',
-      content: '',
-      createdAt: new Date(),
-      updatedAt: null,
+    this.store.addNote(this.currentNote);
+    this.setupNoteUpdate();
+  }
+
+  private setupNoteUpdate(): void {
+    effect(() => {
+      this.noteUpdated();
+      this.currentNote.title = this.noteTitle.value;
+      this.currentNote.content = this.noteContent.value;
+      this.currentNote.updatedAt = new Date();
+      untracked(() => this.store.updateNote(this.currentNote));
     });
   }
 
-  public goToListPage(): void {
-    this.navSrc.toggleNavigationTo.next(NotifyRoutes.LIST);
+  @HostListener('window:beforeunload')
+  public onBeforeUnload(): void {
+    this.cleanupEmptyNote();
+  }
+
+  ngOnDestroy(): void {
+    this.cleanupEmptyNote();
+  }
+
+  private cleanupEmptyNote(): void {
+    const isNoteTitleEmpty = !this.noteTitle.value?.trim();
+    const isNoteContentEmpty = !this.noteContent.value?.trim();
+    const isNoteEmpty = isNoteTitleEmpty && isNoteContentEmpty;
+    if (isNoteEmpty) this.store.deleteNote(this.currentNote.id);
   }
 }
