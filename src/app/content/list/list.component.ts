c12fa03d40cca3a4967507b8b5c54f4c240facf5
ArthurBardakov <artleks@gmail.com>
2024-08-12 23:14:00 +0300

    update list display and routing

--- src/app/content/list/list.component.ts
+++ src/app/content/list/list.component.ts
@@ -1,13 +1,21 @@
 import { Component, inject } from '@angular/core';
 import { NotesStore } from '../../state/notes.store';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
+import { NotifyRoutes } from '../../shared/enums/routes';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
 })
 export class ListComponent {
   public readonly store = inject(NotesStore);
+  private readonly router = inject(Router);
+
+  protected goToNote(noteId: string): void {
+    this.router.navigate(['/' + NotifyRoutes.NEW_NOTE], { queryParams: { id: noteId } });
+  }
 }
