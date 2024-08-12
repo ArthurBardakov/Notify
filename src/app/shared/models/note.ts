f1fc62908cafa59d3070996856daa8d7d83e28dc
ArthurBardakov <artleks@gmail.com>
2024-08-12 18:50:56 +0300

    update note model

--- src/app/shared/models/note.ts
+++ src/app/shared/models/note.ts
@@ -1,8 +1,8 @@
 export class Note {
   constructor(
     public id: string,
-    public title: string,
-    public content: string,
+    public title: string | null,
+    public content: string | null,
     public createdAt: Date,
     public updatedAt: Date | null = null,
   ) {}
