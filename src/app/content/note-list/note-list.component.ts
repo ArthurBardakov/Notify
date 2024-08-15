564ef9bb1e2cee8b01f10419940e808e3b3b82ea
ArthurBardakov <artleks@gmail.com>
2024-08-15 13:46:47 +0300

    add notes dates sort pipe

--- src/app/content/note-list/note-list.component.ts
+++ src/app/content/note-list/note-list.component.ts
@@ -3,13 +3,14 @@ import { NotesStore } from '../../state/notes.store';
 import { MatIconModule } from '@angular/material/icon';
 import { NotifyRoutes } from '../../shared/enums/routes';
 import { NavService } from '../../navbar/nav.service';
+import { SortNotesByDatePipe } from './sort-notes.pipe';
 
 @Component({
   selector: 'app-note-list',
   templateUrl: './note-list.component.html',
   styleUrl: './note-list.component.scss',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, SortNotesByDatePipe],
 })
 export class NoteListComponent {
   public readonly store = inject(NotesStore);
---
+++ src/app/content/note-list/sort-notes.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Note } from '../../shared/models/note';
+
+@Pipe({
+  name: 'sortNotesByDate',
+  pure: true,
+  standalone: true
+})
+export class SortNotesByDatePipe implements PipeTransform {
+
+  transform(notes: Note[]): Note[] {
+    if (!notes || notes.length === 0) return [];
+
+    return notes.sort((a, b) => {
+      const dateA = a.updatedAt ? a.updatedAt : a.createdAt;
+      const dateB = b.updatedAt ? b.updatedAt : b.createdAt;
+
+      return dateB.getTime() - dateA.getTime();
+    });
+  }
+}
