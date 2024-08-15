8f420e3074f026579e5d9e23ec8c1bdf9c25d469
ArthurBardakov <artleks@gmail.com>
2024-08-15 16:44:38 +0300

    group notes by date

--- src/app/content/note-list/note-list.component.ts
+++ src/app/content/note-list/note-list.component.ts
@@ -4,13 +4,22 @@ import { MatIconModule } from '@angular/material/icon';
 import { SortNotesByDatePipe } from './sort-notes.pipe';
 import { NoteCardComponent } from './note-card/note-card.component';
 import { NoteComponent } from "../note/note.component";
+import { GroupNotesByDatePipe } from './group-notes.pipe';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-note-list',
   templateUrl: './note-list.component.html',
   styleUrl: './note-list.component.scss',
   standalone: true,
-  imports: [MatIconModule, SortNotesByDatePipe, NoteCardComponent, NoteComponent],
+  imports: [
+    MatIconModule,
+    SortNotesByDatePipe,
+    NoteCardComponent,
+    NoteComponent,
+    GroupNotesByDatePipe,
+    DatePipe,
+  ],
 })
 export class NoteListComponent {
   public readonly store = inject(NotesStore);
