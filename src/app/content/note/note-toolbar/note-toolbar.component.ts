a7baa3930f24102a2a6c926f2277bc4d8d7447bc
ArthurBardakov <artleks@gmail.com>
2024-08-22 12:48:10 +0300

    update color picker

--- src/app/content/note/note-toolbar/note-toolbar.component.ts
+++ src/app/content/note/note-toolbar/note-toolbar.component.ts
@@ -15,8 +15,15 @@ import { outputFromObservable, toObservable } from '@angular/core/rxjs-interop';
 })
 export class NoteToolbarComponent implements OnInit {
   public containerElement = input.required<ElementRef<HTMLElement>>();
-  protected readonly noteColor = signal<string>(CssVariables.backgroundColor);
+  protected readonly noteColor = signal<string>(CssVariables.primaryColor);
   public readonly selectedNoteColor = outputFromObservable(toObservable(this.noteColor));
+
+  protected readonly colorPickerPalette = [
+    '#ffcdd2', '#f8bbd0', '#e1bee7', '#d1c4e9', '#c5cae9',
+    '#bbdefb', '#b3e5fc', '#b2ebf2', '#b2dfdb', '#c8e6c9',
+    '#dcedc8', '#f0f4c3', '#fff9c4', '#ffecb3', '#ffe0b2',
+    '#ffccbc', '#d7ccc8', '#fafafa', '#cfd8dc', CssVariables.primaryColor
+  ];
 
   public get isMediumEditorToolbarOpen(): boolean {
     const containerElement = this.containerElement().nativeElement;
