d5c05d53786b40c8b1ca8d7c4a8dbbf0e083053d
ArthurBardakov <artleks@gmail.com>
2024-08-22 13:00:14 +0300

    change note card color on note settings

--- src/app/content/note-list/note-card/note-card.component.ts
+++ src/app/content/note-list/note-card/note-card.component.ts
@@ -4,18 +4,18 @@ import { NotesStore } from '../../../state/notes.store';
 import { NavService } from '../../../navbar/nav.service';
 import { NotifyRoutes } from '../../../shared/enums/routes';
 import { Note } from '../../../shared/interfaces/note';
-import { DatePipe } from '@angular/common';
+import { DatePipe, NgStyle } from '@angular/common';
 
 @Component({
     selector: 'app-note-card',
     templateUrl: './note-card.component.html',
     styleUrl: './note-card.component.scss',
     standalone: true,
-    imports: [MatIconModule, DatePipe],
+    imports: [MatIconModule, DatePipe, NgStyle],
 })
 export class NoteCardComponent {
     public note = input.required<Note>();
-    public readonly store = inject(NotesStore);
+    protected readonly store = inject(NotesStore);
     private readonly navSrc = inject(NavService);
   
     protected goToNote(noteId: string): void {
