63faf813ff9bfe1419a5f1def193ddf25fa4b352
ArthurBardakov <artleks@gmail.com>
2024-08-23 16:16:08 +0300

    add console log for mobile devices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { AfterViewInit, Component, ElementRef, signal, viewChild } from '@angular/core';
+import { Component, ElementRef, inject, OnInit, signal, viewChild } from '@angular/core';
 import { NavBarComponent } from './navbar/navbar.component';
 import Hammer from 'hammerjs';
 // do not remove the following import - tests complain if it's not there
 import HammerInput from 'hammerjs';
 import { NgIf } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { environment } from '../environments/environment';
+import eruda from 'eruda';
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Component({
   selector: 'app-root',
@@ -13,16 +16,35 @@ import { RouterModule } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements OnInit {
   private readonly contentElement = viewChild.required<ElementRef<HTMLElement>>('content');
+  
+  private readonly deviceService = inject(DeviceDetectorService);
 
   public readonly swipeLeft = signal<HammerInput | undefined>(undefined);
   public readonly swipeRight = signal<HammerInput | undefined>(undefined);
 
-  ngAfterViewInit(): void {
+  ngOnInit() {
+    this.setupHammerSwipes();
+    this.setupDevelopmentMobileConsole();
+  }
+
+  private setupHammerSwipes(): void {
     const contentElement = this.contentElement().nativeElement;
     const hammer = new Hammer(contentElement);
     hammer.on('swipeleft', (event) => this.swipeLeft.set(event));
     hammer.on('swiperight', (event) => this.swipeRight.set(event));
+  }
+
+  private setupDevelopmentMobileConsole(): void {
+    if (environment.production) return;
+    if (!this.deviceService.isMobile()) return;
+
+    const el = document.createElement('div');
+    document.body.appendChild(el);
+    eruda.init({
+        container: el,
+        tool: ['console', 'elements']
+    });
   }
 }
