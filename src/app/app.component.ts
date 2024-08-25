7b32948e5b9c88e6449ceae82b91562552eed180
ArthurBardakov <artleks@gmail.com>
2024-08-25 18:26:08 +0300

    Deploy Angular project to GitHub Pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,12 @@ import { environment } from '../environments/environment';
 import eruda from 'eruda';
 import { DeviceDetectorService } from 'ngx-device-detector';
 
+// Define the BeforeInstallPromptEvent interface if it's not available in your TypeScript environment
+interface BeforeInstallPromptEvent extends Event {
+  prompt: () => Promise<void>;
+  userChoice: Promise<{ outcome: 'accepted' | 'dismissed' }>;
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -23,10 +29,48 @@ export class AppComponent implements OnInit {
 
   public readonly swipeLeft = signal<HammerInput | undefined>(undefined);
   public readonly swipeRight = signal<HammerInput | undefined>(undefined);
+  private installPrompt?: BeforeInstallPromptEvent | null = null;
+  private installButton!: HTMLElement;
+
 
   ngOnInit() {
     this.setupHammerSwipes();
     this.setupDevelopmentMobileConsole();
+  }
+
+  ngAfterViewInit(): void {
+    const installButton = document.querySelector("#install");
+    console.log(installButton);
+    
+
+    window.addEventListener("beforeinstallprompt", (event) => {
+      event.preventDefault();
+      this.installPrompt = event as BeforeInstallPromptEvent;
+      console.log(event);
+      
+      installButton?.removeAttribute("hidden");
+    });
+    
+  }
+
+  async triggerInstallPrompt() {
+    if (this.installPrompt) {
+      // Show the install prompt
+      await this.installPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      const userChoice = await this.installPrompt.userChoice;
+      console.log(`User choice: ${userChoice.outcome}`);
+      // Clear the saved prompt since it can't be used again
+      this.installPrompt = null;
+    } else {
+      console.log('Install prompt not available');
+    }
+  }
+
+
+  disableInAppInstallPrompt() {
+    this.installPrompt = null;
+    this.installButton.setAttribute("hidden", "");
   }
 
   private setupHammerSwipes(): void {
